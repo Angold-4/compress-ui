faa01437a0b2be324ecac64a8dff4f1a942c80b7
Angold-4 <awang@angold4.org>
2023-04-14 04:41:12 +0000

    changes

--- compress.sh
+++ compress.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 input_name=$1
 
-python scripts/lossyrnn/encoder.py --model scripts/lossyrnn/models/encoder_epoch_00000100.pth --input uploads/$input_name --output codes --iterations 16
-python scripts/lossyrnn/decoder.py --model scripts/lossyrnn/models/decoder_epoch_00000100.pth --input codes.npz --output compress/
+python scripts/lossyrnn/encoder.py --model scripts/lossyrnn/models/encoder_epoch_00000195.pth --cuda --input uploads/$input_name --output codes --iterations 16
+sleep 1
+python scripts/lossyrnn/decoder.py --model scripts/lossyrnn/models/decoder_epoch_00000195.pth  --cuda --input codes.npz --output compress/
